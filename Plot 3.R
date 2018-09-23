da3d63193b6ea29fac4f41784ae612750775ff47
summeryxs <summeryxs@hotmail.com>
2018-09-23 17:58:37 +0800

    Add files via upload

---
+++ Plot 3.R
@@ -0,0 +1,11 @@
+unzip("exdata%2Fdata%2Fhousehold_power_consumption.zip")
+house_hold_power <- read.table("./household_power_consumption.txt",header = TRUE,sep = ";",na.strings = "?")
+house_hold_power_2007 <-subset(house_hold_power, Date == "1/2/2007"|Date =="2/2/2007")
+t <- strptime(paste(house_hold_power_2007$Date, house_hold_power_2007$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+house_hold_power_2007$Date <- t
+png(filename = "Plot 3.png",width = 480, height = 480,bg = "white")
+plot(house_hold_power_2007$Date,house_hold_power_2007$Sub_metering_1,type = "l",xlab = "",ylab = "Energy sub metering")
+with(house_hold_power_2007,lines(Date,Sub_metering_2,col = "red"))
+with(house_hold_power_2007,lines(Date,Sub_metering_3,col = "blue"))
+legend("topright",pch = 1, col = c("black","red","blue"),legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+dev.off()
